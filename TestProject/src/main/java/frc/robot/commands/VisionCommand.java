a1695998b53200796d391ec62b94f9e90a17903c
kaifergerstrom <kaifergerstrom@gmail.com>
2019-01-19 16:12:27 -0500

    requires

--- TestProject/src/main/java/frc/robot/commands/VisionCommand.java
+++ TestProject/src/main/java/frc/robot/commands/VisionCommand.java
@@ -14,6 +14,7 @@ import edu.wpi.first.networktables.EntryListenerFlags;
 
 public class VisionCommand extends Command {
   public VisionCommand() {
+    requires(Robot.vision);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
